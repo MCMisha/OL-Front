0b184c9fb4ad1fdc9722b1b58b0fbe3544717be1
misha <misha.yakushevich4@gmail.com>
2025-02-19 17:16:04 +0100

    OL-Front Fix

--- src/app/admin/admin-panel/admin-panel-performance/admin-panel-performance-routing.module.ts
+++ src/app/admin/admin-panel/admin-panel-performance/admin-panel-performance-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AdminPanelPerformanceComponent} from "./admin-panel-performance.component";
-import {AdminPanelPerformanceNewComponent} from "./admin-panel-performance-new/admin-panel-performance-new.component";
-import {
-  AdminPanelPerformanceEditComponent
-} from "./admin-panel-performance-edit/admin-panel-performance-edit.component";
 
 const routes: Routes = [
-  {path: '', component: AdminPanelPerformanceComponent},
-  {path: 'new', component: AdminPanelPerformanceNewComponent},
-  {path: ':id/edit', component: AdminPanelPerformanceEditComponent}
+  {path: '', component: AdminPanelPerformanceComponent}
 ];
 
 @NgModule({
