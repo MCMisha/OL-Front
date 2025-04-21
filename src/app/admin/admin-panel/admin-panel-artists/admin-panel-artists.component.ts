2d4f1e5773a6370a9ac52b98e8a9924ce1b4d600
misha <misha.yakushevich4@gmail.com>
2025-04-21 18:14:27 +0200

    OL-Front Dodanie nowego punktu w menu

---
+++ src/app/admin/admin-panel/admin-panel-artists/admin-panel-artists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel-artists',
+  templateUrl: './admin-panel-artists.component.html',
+  styleUrl: './admin-panel-artists.component.scss'
+})
+export class AdminPanelArtistsComponent {
+
+}
---
+++ src/app/admin/admin-panel/admin-panel-artists/admin-panel-artists.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminPanelArtistsComponent } from './admin-panel-artists.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminPanelArtistsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminPanelArtistsModule { }
