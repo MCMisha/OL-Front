03077d3f50c40277df2ac55deae8af5c7db447b6
misha <misha.yakushevich4@gmail.com>
2025-11-30 20:20:19 +0100

    OL-Front About artists fix

--- src/app/models/artist.ts
+++ src/app/models/artist.ts
@@ -1,8 +1,10 @@
+import {ArtistCategory} from "./enums/artist-category.enum";
+
 export interface Artist {
   id: number;
   firstName: string;
   lastName: string;
-  category: number;
+  category: ArtistCategory;
   photo?: string;
   description?: string;
 }
