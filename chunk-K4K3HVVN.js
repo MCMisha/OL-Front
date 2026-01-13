bb0e3ba8aba44072dbc9793255c9baa2df2b679e
MCMisha <misha.yakushevich4@gmail.com>
2026-01-13 20:14:34 +0000

    Deploying to gh-pages from @ MCMisha/OL-Front@e86db24dbfe5cc853d00711fa605c894d9495a62 🚀

---
+++ chunk-K4K3HVVN.js
@@ -0,0 +1,1 @@
+import{Rc as o,ba as e,ma as n}from"./chunk-K2A4CEZH.js";var d=(()=>{class t{static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275mod=n({type:t})}static{this.\u0275inj=e({imports:[o]})}}return t})();export{d as AdminPanelContactModule};
